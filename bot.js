6ecefc1aaea9bfeb96b458b0766e0ed376967642
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2016-01-21 14:17:26 +0100

    Refactor bot in smaller modules

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
-var os = require('os');
-var Request = require('superagent');
-var _ = require('underscore');
-
 var Botkit = require('botkit');
 var controller = Botkit.slackbot();
+var answers = require('./lib/answers');
 
 var janitor = controller.spawn({
   token: process.env.token
@@ -14,87 +11,27 @@ janitor.startRTM(function(err,bot,payload) {
   }
 });
 
-var OFFICE_ZIP = 8005;
 
-controller.hears(['shutdown'],'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(
+    ['shutdown', 'get lost', 'self.*destruct', 'destroy'],
+    'direct_message,direct_mention,mention', 
+    answers.shutdown
+);
 
-    bot.startConversation(message,function(err, convo) {
-        convo.ask('Are you sure you want me to shutdown?',[
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    },3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
-            }
-        }
-        ]);
-    });
-});
+controller.hears(
+    ['next( collection)?'],
+    'direct_message,direct_mention,mention',
+    answers.nextCollection
+);
 
-controller.hears(['next collections'],'direct_message,direct_mention,mention',function(bot, message) {
-    // today in YYYY-MM-DD format
-    var today = new Date().toJSON().slice(0, 10);
-    Request
-    .get('http://openerz.herokuapp.com/api/calendar?start=' + today + '&zip=' + OFFICE_ZIP + '&sort=date:asc&limit=3')
-    .end(function(err, res){
-        var collections = '';
-        _.each(res.body.result, function(col) {
-            var colType = col.type.charAt(0).toUpperCase() + col.type.slice(1);
-            collections += '- ' + col.date + ': ' + colType + "\n";
-        });
-        bot.reply(message,':robot_face: Next collections in ' + OFFICE_ZIP + ': :robot_face:' + "\n" + collections);
-    });
-});
+controller.hears(
+    ['(collection )?today'],
+    'direct_message,direct_mention,mention',
+    answers.todayCollection
+);
 
-controller.hears(['collections today'],'direct_message,direct_mention,mention',function(bot, message) {
-    // today in YYYY-MM-DD format
-    var today = new Date().toJSON().slice(0, 10);
-    Request
-    .get('http://openerz.herokuapp.com/api/calendar?start=' + today + '&end=' + today + '&sort=zip:asc')
-    .end(function(err, res){
-        var collections = '';
-        _.each(res.body.result, function(col) {
-            var colType = col.type.charAt(0).toUpperCase() + col.type.slice(1);
-            collections += '- ' + col.zip + ': ' + colType + "\n";
-        });
-        bot.reply(message,':robot_face: All collections today: :robot_face:' + "\n" + collections);
-    });
-});
-
-
-controller.hears(['uptime','identify yourself','who are you','what is your name'],'direct_message,direct_mention,mention',function(bot, message) {
-
-    var hostname = os.hostname();
-    var uptime = formatUptime(process.uptime());
-
-    bot.reply(message,':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-});
-
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime !== 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}
+controller.hears(
+    ['uptime','identify yourself','who are you','what is your name'],
+    'direct_message,direct_mention,mention',
+    answers.uptime
+);
