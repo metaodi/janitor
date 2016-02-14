1389bd8e57ba30355d0243668df488e3116d20d6
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2016-02-14 13:49:01 +0100

    Switch to single `hear` stmt incl. didNotUnderstand call

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var Botkit = require('botkit');
+var _ = require('underscore');
 var controller = Botkit.slackbot();
 var answers = require('./lib/answers');
 
@@ -12,54 +13,91 @@ janitor.startRTM(function(err,bot,payload) {
   }
 });
 
+function matcher(text) {
+    var text = text;
+    return function(pattern) {
+        var re = new RegExp(pattern);
+        return re.test(text);
+    };
+}
 
-controller.hears(
-    ['shutdown', 'get lost', 'self.*destruct', 'destroy', 'shut.*up', 'go away'],
-    'direct_message,direct_mention,mention', 
-    answers.shutdown
-);
 
-controller.hears(
-    ['next.*coll'],
-    'direct_message,direct_mention,mention',
-    answers.nextCollection
-);
+controller.hears(['.*'], ['direct_message,direct_mention'], function(bot, message) {
+    var shutdown = _.any(
+        ['shutdown', 'get lost', 'self.*destruct', 'destroy', 'shut.*up', 'go away'],
+        matcher(message.text)
+    );
+    if (shutdown) {
+        answers.shutdown(bot, message);
+        return;
+    }
 
-controller.hears(
-    ['today'],
-    'direct_message,direct_mention,mention',
-    answers.todayCollection
-);
+    var nextColl = _.any(
+        ['next.*coll'],
+        matcher(message.text)
+    );
+    if (nextColl) {
+        answers.nextCollection(bot, message);
+        return;
+    }
 
-controller.hears(
-    ['board'],
-    'direct_message,direct_mention,mention',
-    answers.stationBoard
-);
+    var todayColl = _.any(
+        ['today'],
+        matcher(message.text)
+    );
+    if (todayColl) {
+        answers.todayCollection(bot, message);
+        return;
+    }
 
-controller.hears(
-    ['next.*conn'],
-    'direct_message,direct_mention,mention',
-    answers.nextConnection
-);
+    var board = _.any(
+        ['board'],
+        matcher(message.text)
+    );
+    if (board) {
+        answers.stationBoard(bot, message);
+        return;
+    }
 
-controller.hears(
-    ['calendar'],
-    'direct_message,direct_mention,mention',
-    answers.nextCalendarEntries
-);
+    var nextConn = _.any(
+        ['next.*conn'],
+        matcher(message.text)
+    );
+    if (nextConn) {
+        answers.nextConnection(bot, message);
+        return;
+    }
+    
+    var calendar = _.any(
+        ['calendar'],
+        matcher(message.text)
+    );
+    if (calendar) {
+        answers.nextCalendarEntries(bot, message);
+        return;
+    }
+    
+    var hello = _.any(
+        ['hi', 'hello', 'hey'],
+        matcher(message.text)
+    );
+    if (hello) {
+        answers.hello(bot, message);
+        return;
+    }
 
-controller.hears(
-    ['hi', 'hello', 'hey'],
-    'direct_message,direct_mention,mention',
-    answers.hello
-);
+    var uptime = _.any(
+        ['uptime', 'identify yourself', 'who are you', 'what is your name', 'what do you do', 'can you help me'],
+        matcher(message.text)
+    );
+    if (uptime) {
+        answers.uptime(bot, message);
+        return;
+    }
 
-controller.hears(
-    ['uptime', 'identify yourself', 'who are you', 'what is your name', 'what do you do', 'can you help me'],
-    'direct_message,direct_mention,mention',
-    answers.uptime
-);
+    answers.didNotUnderstand(bot, message);
+});
+
 
 // To keep Heroku's free dyno awake
 http.createServer(function(request, response) {
