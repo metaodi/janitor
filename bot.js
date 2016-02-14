93c482cc26ed2288257baed9ad72c5b4404b7bdb
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2016-02-14 14:09:36 +0100

    Refactor single hear statement with config object

--- bot.js
+++ bot.js
@@ -21,81 +21,54 @@ function matcher(text) {
     };
 }
 
+var requestConfig = [
+    {
+        'pattern': ['shutdown', 'get lost', 'self.*destruct', 'destroy', 'shut.*up', 'go away'],
+        'answerFn': answers.shutdown
+    },
+    {
+        'pattern': ['next.*coll'],
+        'answerFn': answers.nextCollection
+    },
+    {
+        'pattern': ['today'],
+        'answerFn': answers.todayCollection
+    },
+    {
+        'pattern': ['board'],
+        'answerFn': answers.stationBoard
+    },
+    {
+        'pattern': ['next.*conn'],
+        'answerFn': answers.nextConnection
+    },
+    {
+        'pattern': ['calendar'],
+        'answerFn': answers.nextCalendarEntries
+    },
+    {
+        'pattern': ['hi', 'hello', 'hey'],
+        'answerFn': answers.hello
+    },
+    {
+        'pattern': ['uptime', 'identify yourself', 'who are you', 'what is your name', 'what do you do', 'can you help me'],
+        'answerFn': answers.uptime
+    },
+];
+
 
 controller.hears(['.*'], ['direct_message,direct_mention'], function(bot, message) {
-    var shutdown = _.any(
-        ['shutdown', 'get lost', 'self.*destruct', 'destroy', 'shut.*up', 'go away'],
-        matcher(message.text)
-    );
-    if (shutdown) {
-        answers.shutdown(bot, message);
-        return;
+    var noAnswer = _.every(requestConfig, function(request) {
+        var matched = _.any(request.pattern, matcher(message.text));
+        if (matched) {
+            request.answerFn(bot, message);
+            return false; //break out of every()
+        }
+        return true; // continue with next requestConfig
+    });
+    if (noAnswer) {
+        answers.didNotUnderstand(bot, message);
     }
-
-    var nextColl = _.any(
-        ['next.*coll'],
-        matcher(message.text)
-    );
-    if (nextColl) {
-        answers.nextCollection(bot, message);
-        return;
-    }
-
-    var todayColl = _.any(
-        ['today'],
-        matcher(message.text)
-    );
-    if (todayColl) {
-        answers.todayCollection(bot, message);
-        return;
-    }
-
-    var board = _.any(
-        ['board'],
-        matcher(message.text)
-    );
-    if (board) {
-        answers.stationBoard(bot, message);
-        return;
-    }
-
-    var nextConn = _.any(
-        ['next.*conn'],
-        matcher(message.text)
-    );
-    if (nextConn) {
-        answers.nextConnection(bot, message);
-        return;
-    }
-    
-    var calendar = _.any(
-        ['calendar'],
-        matcher(message.text)
-    );
-    if (calendar) {
-        answers.nextCalendarEntries(bot, message);
-        return;
-    }
-    
-    var hello = _.any(
-        ['hi', 'hello', 'hey'],
-        matcher(message.text)
-    );
-    if (hello) {
-        answers.hello(bot, message);
-        return;
-    }
-
-    var uptime = _.any(
-        ['uptime', 'identify yourself', 'who are you', 'what is your name', 'what do you do', 'can you help me'],
-        matcher(message.text)
-    );
-    if (uptime) {
-        answers.uptime(bot, message);
-        return;
-    }
-
-    answers.didNotUnderstand(bot, message);
 });
 
 
